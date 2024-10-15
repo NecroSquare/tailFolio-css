4ccfa001c34a889dc7823095d004016679792033
NecroSquare <mfabioalfarizie1@gmail.com>
2024-10-15 16:37:29 +0700

    darkmode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,13 +2,18 @@
 window.onscroll = function(){
     const header = document.querySelector('header')
     const fixNav = header.offsetTop
+    const back = document.querySelector('#back')
     
     console.log('Window Y offset:', window.scrollY) // Check the scroll position
     
     if(window.scrollY > fixNav) {
         header.classList.add('nav-fix')
+        back.classList.remove('hidden')
+        back.classList.add('flex')
     }else {
         header.classList.remove('nav-fix')
+        back.classList.add('hidden')
+        back.classList.remove('flex')
     }
     
     }
@@ -21,6 +26,14 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('ham-active')
     navMenu.classList.toggle('hidden')
 })
+
+// When click outside the menu, it will be hidden
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target != navMenu){
+    hamburger.classList.remove('ham-active')
+    navMenu.classList.add('hidden')
+    }
+});
 
 const checkbox = document.querySelector
     ('#toggle')
